bcb953c0997b5059a9aa987cbb4d54ff54dd1967
dwicao <lutfiandwica@gmail.com>
2017-05-27 10:53:48 +0700

    resolving with data

--- schema/schema.js
+++ schema/schema.js
@@ -1,16 +1,22 @@
 const graphql = require('graphql');
-
+const _ = require('lodash');
 const {
   GraphQLObjectType,
-  GraphQlString,
+  GraphQLString,
   GraphQLInt,
+  GraphQLSchema,
 } = graphql;
+
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '47', firstName: 'Samantha', age: 21 },
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
-    id: { type: GraphQlString },
-    firstName: { tyoe: GraphQlString },
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
   },
 });
@@ -20,10 +26,14 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     user: {
       type: UserType,
-      args: { id: { type: GraphQlString } },
+      args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-
+        return _.find(users, { id: args.id });
       },
     },
   },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
